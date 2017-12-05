e53cdc78ebabdaad8f4f817bcd164323d7a9d438
KenSv <sskkru@yahoo.com>
2017-12-05 17:52:54 +0300

    Исправлена ошибка смещения массива фильтрованных данных Заменен тип char на _u8 в буферах

--- main.cpp
+++ main.cpp
@@ -3,139 +3,129 @@
 #include <string.h>
 #include <unistd.h>
 #include "db.h"
+#include "include/Riq.h"
 
 using namespace std;
 
-void getType2(char** buf) {
-    unsigned int varType = *((int *) *buf);
-    string typeName = "";
-    if(varType & (DBIT_ARRAY|DBIT_ARRAY2|DBIT_ARRAY3))
-    {
-        typeName = "A";
-        switch (varType & 0x30000000)
-        {
-        case DBIT_ARRAY2:
-            typeName += "2D";
-            break;
-        case DBIT_ARRAY3:
-            typeName += "3D";
-            break;
-        default:
-            typeName += "1D";
-            break;
-        }
-    } else {
-        typeName = "  N";
-    }
-
-    typeName += ((varType & 0xC0000000)? "U": "S");
-    unsigned int nb = (varType >> 24) & 0x0F;
-//    unsigned int id = varType & 0x000000FF;
-//    cout << "Id: " << id << " bytes: " << hex << nb <<endl;
-
-    typeName +=  (nb == 1? "0": "\0") + to_string(nb << 3);
-
-    *buf += 4;  // указатель на значение переменной
-    long value;
-    int arItems;
-    if (typeName[0] == 'A')
-    {
-        arItems = *((int *) *buf);
-//        cout << "array bytes: " << nb << endl;
-        *buf += sizeof(int) + arItems * nb;  // длина массива следует за типом значения - 4 байта?
-        value = arItems;
-    }
-    else
-    {
-        switch (nb)
-        {
-        case 1:
-            value = *((char *) *buf);
-            break;
-        case 2:
-            value = *((short *) *buf);
-            break;
-        case 4:
-            value = *((int *) *buf);
-            break;
-        case 8:
-            value = *((long *) *buf);
-            break;
-        default:
-            break;
-        }
-        *buf += nb;
-    }
-
-
-    // << hex << (long) (*buf)
-    cout << "Тип: " << typeName << " Код типа: 0x" << hex << varType << " Значение: " << value << endl;
-
-}
-
-
-
 int main(int argc, char* argv[])
 {
-   char dirName[1000];
-//   char ch; // для варианта с побайтовой обработкой
-   long int length;
-   ifstream in;
-   ofstream out;
-   char *fname = new char [28];
-   cout << "Current path: " << getcwd(dirName, sizeof(dirName)) << endl;
+    char dirName[1000];
+    long int length;
+    FILE *fRiq, *fIn, *fOut;
+    _u8* pRiq;
+    char *fname = new char [30];
+
+    printf("Current path: %s\n", getcwd(dirName, sizeof(dirName)));
     if (argc > 1) {
         fname = argv[1];
     } else {
         strcpy(fname, "2017_09_13_12_54_32_333.riq");
     }
-   cout << "File name: " << fname << endl;
-   in.open(fname, ios::in | ios::binary);
-   out.open("out.riq", ios::out | ios::binary);
-
-   if (!in) {
-       length = in.tellg();
-       cout << "Error opening file ";
-       exit(1);
-   }
-
-// вариант с побайтовым чтением
-/*
-    while(in){
-        in.get(ch);
-        out.put(ch);
+    printf("File name: %s\n", fname);
+    try
+    {
+        fIn = fopen(fname, "rb");
     }
-*/
+    catch (int e)
+    {
+        printf("Error opening file. Error %i", e);
+//        fclose(fIn);
+        exit(1);
+    }
+    fOut = fopen("origin.riq", "wb");
+    fseek(fIn, 0L, SEEK_END);
+    length = ftell(fIn);
 
-// вариант с чтением/записью посредством буфера
-    in.seekg(0, in.end);
-    length = in.tellg();
-    cout << "File length: " << length << endl;
-    char *buf = new char [length];
+    printf("File length: %li\n", length);
+    printf("===========================================\n");
+    printf("  Адрес |\tЗначение\tОписание\n");
+    printf("===========================================\n");
+
+    _u8 *buf = new _u8 [length];
     try {
-        in.seekg(0);
-        in.read((char *) buf, length);
-//        in.read((char *) & str, sizeof(str_type));     // вариант с чтением структуры str_type
-    char* readPtr = buf;
-//	int ptr = 0;
-	while(readPtr < &buf[length]) {
-//	    for(int n=0; n < 50; n++)
-//	    {
-//            getType(&ptr, buf);
-            getType2(&readPtr);
-//	    }
-	}
+        rewind(fIn);
+        fread((char *) buf, sizeof(char), length, fIn);
+        pRiq = (_u8*) malloc(sizeof(_u8)*length);
+        _u8* readPtr = buf;
+        _u8* writePtr = pRiq;
 
-        out.write(buf, length);
-// жаль нет finally. Остаток кода нужно бы поместить туда :)
-// Если через класс, то уничтожать переменные/буферы в деструкторе ?
+        fRiq = fopen("filtered.riq", "w");
+        while(readPtr < &buf[length]) {
+            printf("%8x ", (unsigned int) (readPtr -buf));
+            if(!parseVar(&readPtr, &writePtr))
+                parseArray(&readPtr, &writePtr);
+        }
+        fwrite(buf, sizeof(char), length, fOut);
+        fwrite(pRiq, sizeof(char), length, fRiq);
+
         delete [] buf;
-        in.close();
-        out.close();
+        fclose(fIn);
+        fclose(fOut);
+        fclose(fRiq);
+        free(pRiq);
     } catch (int e) {
-        cout << "An exception " << e <<" occurred\n";
-        exit(2); // указать правильный код ошибки
+        printf("An exception %i occurred\n", e);
+        exit(1);
     }
-
    exit(0);
 }
+
+
+//void getType(char** buf) {
+//    unsigned int varType = *((int *) *buf);
+//    string typeName = "";
+//    if(varType & (DBIT_ARRAY|DBIT_ARRAY2|DBIT_ARRAY3))
+//    {
+//        typeName = "A";
+//        switch (varType & 0x30000000)
+//        {
+//        case DBIT_ARRAY2:
+//            typeName += "2D";
+//            break;
+//        case DBIT_ARRAY3:
+//            typeName += "3D";
+//            break;
+//        default:
+//            typeName += "1D";
+//            break;
+//        }
+//    } else {
+//        typeName = "  N";
+//    }
+//
+//    typeName += ((varType & 0xC0000000)? "U": "S");
+//    unsigned int nb = (varType >> 24) & 0x0F;
+//    typeName +=  (nb == 1? "0": "\0") + to_string(nb << 3);
+//
+//    *buf += 4;  // указатель на значение переменной
+//    long value;
+//    int arItems;
+//    if (typeName[0] == 'A')
+//    {
+//        arItems = *((int *) *buf);
+//        *buf += sizeof(int) + arItems * nb;  // длина массива следует за типом значения - 4 байта?
+//        value = arItems;
+//    }
+//    else
+//    {
+//        switch (nb)
+//        {
+//        case 1:
+//            value = *((char *) *buf);
+//            break;
+//        case 2:
+//            value = *((short *) *buf);
+//            break;
+//        case 4:
+//            value = *((int *) *buf);
+//            break;
+//        case 8:
+//            value = *((long *) *buf);
+//            break;
+//        default:
+//            break;
+//        }
+//        *buf += nb;
+//    }
+//    printf("Тип: %s\t Код типа: %#08x\t Значение: %li\n", &typeName[0], varType, value);
+//}
